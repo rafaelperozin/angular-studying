9064c0feef6f5bd083d923086150a7e477c6fe06
bradev <rafaelperozin@outlook.com>
2019-07-14 21:33:19 +0100

    directives: ngIf

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
   serverName = 'Test Server';
+  serverCreated = false;
 
   constructor() {
     setTimeout(() => {
@@ -26,6 +27,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreationServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
